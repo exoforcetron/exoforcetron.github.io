ea663b3cc62bb11e1a45a64fad808093c28b8a2d
exoforcetron <52507848+exoforcetron@users.noreply.github.com>
2020-04-22 10:42:13 -0700

    Update hover.js

--- javascript/hover.js
+++ javascript/hover.js
@@ -1,17 +1,36 @@
-function hovered() {
-//    this.style.backgroundColor = "#0000ff";
-  document.getElementById("teddyPng").style.opacity="0";
+function hovered1(){
+    document.getElementById("teddyPng").style.opacity = "0";
+    document.getElementById("hov1").style.background="black";
+    document.getElementById("hov1").style.opacity = "60%";
+    document.getElementById("Kin").style.opacity = "1";
+}
+function done1(){
+    document.getElementById("teddyPng").style.opacity = "1";
+    document.getElementById("Kin").style.opacity = "0";
+    document.getElementById("hov1").style.background="none";
 }
 
-function off() {
-//    this.style.backgroundColor = "#ff0000";
-  document.getElementById("teddyPng").style.opacity="1";
-
+function hovered2(){
+    document.getElementById("VMFPng").style.opacity = "0";
+    document.getElementById("hov2").style.background="black";
+    document.getElementById("hov2").style.opacity = "60%";
+    document.getElementById("Ani").style.opacity = "1";
+}
+function done2(){
+    document.getElementById("VMFPng").style.opacity = "1";
+    document.getElementById("Ani").style.opacity = "0";
+    document.getElementById("hov2").style.background="none";
 }
 
-document.getElementById("teddyImg").addEventListener("mouseover",hovered);
-document.getElementById("teddyImg").addEventListener("mouseout",off);
+function hovered3(){
+//    document.getElementById("proPng").style.opacity = "0";
+    document.getElementById("hov3").style.background="black";
+    document.getElementById("hov3").style.opacity = "60%";
+    document.getElementById("pro").style.opacity = "1";
+}
+function done3(){
+//    document.getElementById("ProPng").style.opacity = "1";
+    document.getElementById("pro").style.opacity = "0";
+    document.getElementById("hov3").style.background="none";
+}
 
-//function userHover() {
-//  document.getElementById("teddyImg").style.backgroundColor= "black";
-//}
